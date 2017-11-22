c76b8749c1449327debc22169df01a391d6a9e14
Vincent Force <vforce@free.fr>
2017-11-22 20:28:18 +0100

    fixed small legend issues

--- plot4.R
+++ plot4.R
@@ -8,7 +8,7 @@ hpc$POSIXlt <- strptime(paste(hpc$Date, hpc$Time, sep = "T"), "%d/%m/%YT%T")
 
 par(mfrow = c(2, 2))
 
-plot(hpc$POSIXlt, hpc$Global_active_power, type="l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(hpc$POSIXlt, hpc$Global_active_power, type="l", xlab = "", ylab = "Global Active Power")
 
 plot(hpc$POSIXlt, hpc$Voltage, type="l", xlab = "datetime", ylab = "Voltage")
 
@@ -17,7 +17,7 @@ points(hpc$POSIXlt, hpc$Sub_metering_2, type="l", xlab = "", ylab = "", col = "r
 points(hpc$POSIXlt, hpc$Sub_metering_3, type="l", xlab = "", ylab = "", col = "blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"),
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), bty = "n",
-       xjust = 1, yjust = 1)
+       cex = 1)
 
 plot(hpc$POSIXlt, hpc$Global_reactive_power, type="l", xlab = "datetime", ylab = "Global_reactive_power")
 
